ec45da184697ab0f1c4bbb407e1f381109b4ea7f
Seweryn Banasiak <seweryn.banasiak@stud.pwsz.legnica.edu.pl>
2022-01-05 18:18:32 +0100

    Updated MainMenu script for existing buttons

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public string mainScene;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(mainScene);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }

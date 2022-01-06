cc98fa6f859926b6cefc94f6008bbdecf971907c
Seweryn Banasiak <seweryn.banasiak@stud.pwsz.legnica.edu.pl>
2022-01-06 22:24:57 +0100

    Scripted options and back button

---
+++ Assets/Scripts/Menu/Button_Back.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class Button_Back : ButtonBase
+{
+    public GameObject optionsScreen;
+    public override void Click()
+    {
+        optionsScreen.SetActive(false);
+    }
+}
